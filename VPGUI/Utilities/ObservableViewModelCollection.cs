78db90a095bebf4e84b377d306d36bcd21409632
asarium <asarium@gmail.com>
2013-02-20 20:19:50 +0100

    Move the ObservableViewModelCollection to its own file.

---
+++ VPGUI/Utilities/ObservableViewModelCollection.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace VPGUI.Utilities
+{
+    public sealed class ObservableViewModelCollection<TViewModel, TModel> : ObservableCollection<TViewModel>
+    {
+        private readonly ObservableCollection<TModel> _source;
+        private readonly Func<TModel, TViewModel> _viewModelFactory;
+
+        private Dispatcher EventDispatcher
+        {
+            get; set;
+        }
+
+        public ObservableViewModelCollection(ObservableCollection<TModel> source,
+                                             Func<TModel, TViewModel> viewModelFactory)
+            : base(source.Select(viewModelFactory))
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(viewModelFactory != null);
+
+            this._source = source;
+            this._viewModelFactory = viewModelFactory;
+            this._source.CollectionChanged += this.OnSourceCollectionChanged;
+
+            EventDispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        private TViewModel CreateViewModel(TModel model)
+        {
+            return this._viewModelFactory(model);
+        }
+
+        private void OnSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            // Do this in the UI thread or else an exception is thrown.
+            EventDispatcher.InvokeAsync(() =>
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        for (int i = 0; i < e.NewItems.Count; i++)
+                        {
+                            this.Insert(e.NewStartingIndex + i, this.CreateViewModel((TModel) e.NewItems[i]));
+                        }
+                        break;
+
+                    case NotifyCollectionChangedAction.Move:
+                        if (e.OldItems.Count == 1)
+                        {
+                            this.Move(e.OldStartingIndex, e.NewStartingIndex);
+                        }
+                        else
+                        {
+                            var items = this.Skip(e.OldStartingIndex).Take(e.OldItems.Count).ToList();
+                            for (int i = 0; i < e.OldItems.Count; i++)
+                            {
+                                this.RemoveAt(e.OldStartingIndex);
+                            }
+
+                            for (int i = 0; i < items.Count; i++)
+                            {
+                                this.Insert(e.NewStartingIndex + i, items[i]);
+                            }
+                        }
+                        break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        for (int i = 0; i < e.OldItems.Count; i++)
+                        {
+                            this.RemoveAt(e.OldStartingIndex);
+                        }
+                        break;
+
+                    case NotifyCollectionChangedAction.Replace:
+                        // remove
+                        for (int i = 0; i < e.OldItems.Count; i++)
+                        {
+                            this.RemoveAt(e.OldStartingIndex);
+                        }
+
+                        // add
+                        goto case NotifyCollectionChangedAction.Add;
+
+                    case NotifyCollectionChangedAction.Reset:
+                        this.Clear();
+                        foreach (var t in e.NewItems)
+                        {
+                            this.Add(this.CreateViewModel((TModel) t));
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }, DispatcherPriority.Background);
+        }
+    }
+}
--- VPGUI/Utilities/Util.cs
+++ VPGUI/Utilities/Util.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Diagnostics;
-using System.Diagnostics.Contracts;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -14,89 +10,6 @@ using IWin32Window = System.Windows.Forms.IWin32Window;
 namespace VPGUI.Utilities
 {
     // From http://stackoverflow.com/questions/1256793/mvvm-sync-collections
-    public sealed class ObservableViewModelCollection<TViewModel, TModel> : ObservableCollection<TViewModel>
-    {
-        private readonly ObservableCollection<TModel> _source;
-        private readonly Func<TModel, TViewModel> _viewModelFactory;
-
-        public ObservableViewModelCollection(ObservableCollection<TModel> source,
-                                             Func<TModel, TViewModel> viewModelFactory)
-            : base(source.Select(viewModelFactory))
-        {
-            Contract.Requires(source != null);
-            Contract.Requires(viewModelFactory != null);
-
-            this._source = source;
-            this._viewModelFactory = viewModelFactory;
-            this._source.CollectionChanged += this.OnSourceCollectionChanged;
-        }
-
-        private TViewModel CreateViewModel(TModel model)
-        {
-            return this._viewModelFactory(model);
-        }
-
-        private void OnSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            switch (e.Action)
-            {
-                case NotifyCollectionChangedAction.Add:
-                    for (int i = 0; i < e.NewItems.Count; i++)
-                    {
-                        this.Insert(e.NewStartingIndex + i, this.CreateViewModel((TModel) e.NewItems[i]));
-                    }
-                    break;
-
-                case NotifyCollectionChangedAction.Move:
-                    if (e.OldItems.Count == 1)
-                    {
-                        this.Move(e.OldStartingIndex, e.NewStartingIndex);
-                    }
-                    else
-                    {
-                        var items = this.Skip(e.OldStartingIndex).Take(e.OldItems.Count).ToList();
-                        for (int i = 0; i < e.OldItems.Count; i++)
-                        {
-                            this.RemoveAt(e.OldStartingIndex);
-                        }
-
-                        for (int i = 0; i < items.Count; i++)
-                        {
-                            this.Insert(e.NewStartingIndex + i, items[i]);
-                        }
-                    }
-                    break;
-
-                case NotifyCollectionChangedAction.Remove:
-                    for (int i = 0; i < e.OldItems.Count; i++)
-                    {
-                        this.RemoveAt(e.OldStartingIndex);
-                    }
-                    break;
-
-                case NotifyCollectionChangedAction.Replace:
-                    // remove
-                    for (int i = 0; i < e.OldItems.Count; i++)
-                    {
-                        this.RemoveAt(e.OldStartingIndex);
-                    }
-
-                    // add
-                    goto case NotifyCollectionChangedAction.Add;
-
-                case NotifyCollectionChangedAction.Reset:
-                    this.Clear();
-                    for (int i = 0; i < e.NewItems.Count; i++)
-                    {
-                        this.Add(this.CreateViewModel((TModel) e.NewItems[i]));
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-    }
 
     public static class Util
     {
